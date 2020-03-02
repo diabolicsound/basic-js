0e35ac92df48ccc9e6c1bc40668cd156c359021b
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-02 20:49:35 +0300

    Complete task Count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,11 @@
 module.exports = function countCats(matrix) {
   let score = 0;
-  let arr =  matrix.flat(Infinity);
-  for(let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i] == true)) {
-      arr = arr.flat(Infinity);
-    }
-     else if (arr[i] == '^^') {
-      score =+ 1;
-    }
+  let arr =  matrix.flat();
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == "^^") {
+      score += 1;
+    } 
   }
-  
   return score;
 
-}
+  }
