3b4feeae1f6046ab450d28067944b97cda6d4d3e
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-03 17:49:40 +0300

    complete task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+    if (options.hasOwnProperty('separator') == false &&  options.hasOwnProperty('additionSeparator') == false) {
+ let rep = (str + "+").repeat(options.repeatTimes - 1);
+  return rep + str;
+    }
+    else if (options.hasOwnProperty('additionSeparator') == false && options.hasOwnProperty('separator') == true && options.hasOwnProperty('addition') == false) {
+    let rep2 = (str + options.separator).repeat(options.repeatTimes - 1);
+    return rep2 + str;
+    }
+    else if (options.hasOwnProperty('separator') == true && options.hasOwnProperty('addition') == true && options.hasOwnProperty('additionSeparator') == false) {
+        if (options.additionRepeatTimes == 1) {
+            let part3 = options.addition + options.separator;
+            return (str + part3).repeat(options.repeatTimes - 1) + str + options.addition;
+        }
+        else {
+        let part3 = (options.addition + options.separator).repeat(options.additionRepeatTimes - 1);
+        return (str + part3 + options.separator).repeat(options.repeatTimes - 1) + str + options.addition;
+    }
+}
+else if (options.hasOwnProperty('separator') == true && options.hasOwnProperty('addition') == true && options.hasOwnProperty('additionSeparator') == true) {
+   let part4 = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;
+   let full = (str + part4 + options.separator).repeat(options.repeatTimes - 1) + str + part4;
+   return full;
+}
+}
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,14 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+    let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']   
+    newarr = [];
+    for (let i = 0; i < arr.length; i++) {
+    newarr.push(arr[i].charCodeAt - 96);
     }
-
+    
+    }
     decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+        
     }
 }
 
