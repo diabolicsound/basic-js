a4532b9f5aa34ba7b6e17a50e25b9706dca2d7bc
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-10 17:21:55 +0300

    feat: complete task recutsive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -8,12 +8,18 @@ class DepthCalculator {
     calculateDepth(arr) {
         let newarr = [];
         let a = 1;
-        let depth = 1
+        let depth = 1;
+        let value = 1;
        arr.forEach(element => {
+           value = depth;
+           depth = 1;
         if (typeof(element) == 'object') {
-            depth += this.calculateDepth(element);
-            ;    
-       }});
+            depth += this.calculateDepth(element);  
+       }
+       if (depth < value) {
+       depth = value;
+       }
+    });
        return depth;
     }
     
