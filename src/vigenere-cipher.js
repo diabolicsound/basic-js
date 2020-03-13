fa9cb98f453515534f03ffa7a31d7922b5d1c17e
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-13 17:13:57 +0300

    Complete 4/5 tests from cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,15 +1,61 @@
 class VigenereCipheringMachine {
     encrypt(message, key) {
-    let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']   
-    newarr = [];
-    for (let i = 0; i < arr.length; i++) {
-    newarr.push(arr[i].charCodeAt - 96);
+    let newarr = [];
+    let mesarr = message.toLowerCase().split('');
+    let keyarr  = key.toLowerCase().split('');
+    let newkeyarr = keyarr.slice();
+    for (let i = 0; i < (mesarr.length - newkeyarr.length); i++) {
+        keyarr.push(keyarr[i])
     }
     
-    }
-    decrypt() {
-        
-    }
-}
+    for (let j = 0; j < mesarr.length; j++) {
+        if ((mesarr[j].charCodeAt() < 97 ) && (mesarr[j].charCodeAt() !== 32 ) || (mesarr[j].charCodeAt() > 122 ) ) {
+            newarr.push(mesarr[j]);
+        }
+        else if (((mesarr[j].charCodeAt() + keyarr[j].charCodeAt()) >= 220) && (mesarr[j].charCodeAt() !== 32 )){
+            newarr.push(String.fromCharCode(mesarr[j].charCodeAt() + keyarr[j].charCodeAt() - 123))
+        }
+        else if ((mesarr[j].charCodeAt() == 32 )) {
+            keyarr.splice(j - 1, 0, ' ')
+            newarr.push(String.fromCharCode(mesarr[j].charCodeAt()))
+        }
 
+        else {
+            newarr.push(String.fromCharCode(mesarr[j].charCodeAt() + keyarr[j].charCodeAt() - 97))
+        }
+    }
+        return newarr.join('').toUpperCase(); 
+    }
+    decrypt(message, key) {
+        let newarr = [];
+        let mesarr = message.toLowerCase().split('');
+        let keyarr  = key.toLowerCase().split('');
+        let newkeyarr = keyarr.slice();
+        for (let i = 0; i < (mesarr.length - newkeyarr.length); i++) {
+            keyarr.push(keyarr[i])
+        }
+        
+        for (let j = 0; j < mesarr.length; j++) {
+            if ((mesarr[j].charCodeAt() < 97 ) && (mesarr[j].charCodeAt() !== 32 ) || (mesarr[j].charCodeAt() > 122 ) ) {
+                newarr.push(mesarr[j]);
+            }
+            else if (((mesarr[j].charCodeAt() - keyarr[j].charCodeAt()) < 0) && (mesarr[j].charCodeAt() !== 32 )){
+                newarr.push(String.fromCharCode(mesarr[j].charCodeAt() - keyarr[j].charCodeAt() + 123))
+            } 
+            else if ((mesarr[j].charCodeAt() == 32 )) {
+                keyarr.splice(j - 1, 0, ' ')
+                newarr.push(String.fromCharCode(mesarr[j].charCodeAt()))
+            }
+    
+            else {
+                newarr.push(String.fromCharCode(mesarr[j].charCodeAt() - keyarr[j].charCodeAt() + 97))
+            }
+        }
+        console.log(newarr)
+        console.log(mesarr)
+        console.log(keyarr)
+        return newarr.join('').toUpperCase();
+    }
+
+}
 module.exports = VigenereCipheringMachine;
